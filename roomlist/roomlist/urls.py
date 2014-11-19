4349a1288811cf04f57623b6e8e93cd55fc915ab
Jason Yeomans <jyeo098@gmail.com>
2014-11-18 20:36:26 -0500

    Add lots of user stuff... still having issues

--- roomlist/roomlist/urls.py
+++ roomlist/roomlist/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout_view, name='logout_view'),
     url(r'^buildings/$', views.buildings, name='buildings'),
     url(r'^building/(?P<buildingName>[a-zA-Z]+)$', views.building, name='building'),
 )
--- roomlist/roomlist/views.py
+++ roomlist/roomlist/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import RequestContext, loader
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, logout
 
 from api.models import Building, Room
 
@@ -30,7 +32,33 @@ def building(request, buildingName):
     return HttpResponse(template.render(context))
 
 def login(request):
-    template = loader.get_template('login.html')
-    context = RequestContext(request, {
-    })
-    return HttpResponse(template.render(context))
+     # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = AuthenticationForm(data = request.POST)
+        # check whether it's valid:
+        form.is_valid()
+        form_data = form.clean()
+        user = form.get_user()
+        if user is not None:
+            # the password verified for the user
+            if user.is_active:
+                print("User is valid, active and authenticated")
+            else:
+                print("The password is valid, but the account has been disabled!")
+        else:
+            # the authentication system was unable to verify the username and password
+            print("The username and password were incorrect.")
+        return HttpResponseRedirect('/')
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = AuthenticationForm()
+        template = loader.get_template('login.html')
+        context = RequestContext(request, {
+            'form' : form,
+        })
+        return HttpResponse(template.render(context))
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
