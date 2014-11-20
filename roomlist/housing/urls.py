950c30617ff037480e89238d281ef44bfdd1356f
Daniel W Bond <dbond2@gmu.edu>
2014-11-20 14:56:12 -0500

    url separation, added django auth login

--- roomlist/housing/urls.py
+++ roomlist/housing/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout_view, name='logout_view'),
     url(r'^buildings/$', views.buildings, name='buildings'),
     url(r'^building/(?P<buildingName>[a-zA-Z]+)$', views.building, name='building'),
 )
--- roomlist/settings/urls.py
+++ roomlist/settings/urls.py
@@ -4,10 +4,19 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
+    # project-level urls
+    # url(r'^$', index.html
+    # url(r'^$', about.html
+
+    # login and logout
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'logout/$', 'django.contrib.auth.views.logout', {'template_name': 'index.html'}, name='logout'),
+
+    # app-level urls
+    url(r'^housing/', include('housing.urls')),
+
+    # alternate interfaces
     url(r'^api/', include('api.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('housing.urls')),
 )
