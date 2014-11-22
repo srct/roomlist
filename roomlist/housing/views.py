65ed4e15455e29334e487d57a3665049de2fba3a
Daniel W Bond <dbond2@gmu.edu>
2014-11-22 17:13:36 -0500

    outlined class based views for housing app

--- roomlist/housing/views.py
+++ roomlist/housing/views.py
@@ -4,9 +4,36 @@ from django.template import RequestContext, loader
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, logout
 
-from housing.models import Building, Room
+from braces.views import LoginRequiredMixin
 
-# Create your views here.
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+
+from housing.models import Building, Room, Student
+# a list of neighborhoods and their buildings
+class ListBuildings(LoginRequiredMixin, ListView):
+    model = Building
+    login_url = '/'
+
+# building floors, other information
+class DetailBuilding(LoginRequiredMixin, DetailView):
+    model = Building
+    login_url = '/'
+
+# this lists the rooms on the floor
+class ListRooms(LoginRequiredMixin, ListView):
+    model = Room
+    login_url = '/'
+
+# this lists students in a room
+class DetailRoom(LoginRequiredMixin, ListView):
+    model = Room
+    login_url = '/'
+
+# details about the student
+class DetailStudent(LoginRequiredMixin, DetailStudent):
+    model = Student
+    login_url = '/'
+
 def index(request):
     template = loader.get_template('index.html')
     context = RequestContext(request, {
