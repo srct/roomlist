ad18af1ba8c5d475f5b0dab29ea7475411b85d76
Daniel W Bond <dbond2@gmu.edu>
2014-11-22 18:47:05 -0500

    logout and login urls

--- roomlist/settings/urls.py
+++ roomlist/settings/urls.py
@@ -1,14 +1,23 @@
-from django.views.generic import TemplateView
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
+
+# custom 404
+# custom 500
 
 urlpatterns = patterns('',
 
     # project-level urls
     url(r'^$', TemplateView.as_view( template_name="index.html" ), name='index'),
-    # url(r'^$', about.html
+    # url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    # url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    # url(r'^privacy/$', TemplateView.as_view(template_name='privacy.html'), name='privacy'),
+
+    # logins
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'logout/$', 'django.contrib.auth.views.logout', {'template_name': 'index.html'}, name='logout'),
 
     # app-level urls
     url(r'^housing/', include('housing.urls')),
