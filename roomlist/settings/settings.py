93473bd11392c37a57306e61663a1eafb5541ca1
Jason D Yeomans <jyeoman2@gmu.edu>
2014-11-24 16:09:25 -0500

    moved arround student detail to accounts app

--- roomlist/settings/settings.py
+++ roomlist/settings/settings.py
@@ -15,6 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
     os.path.join(BASE_DIR, 'housing/templates'),
+    os.path.join(BASE_DIR, 'accounts/templates'),
 )
 
 STATICFILES_DIRS = (
