cd621477bd4372f96b8a1c8d05a13b75ed8daad8
Daniel W Bond <dbond2@gmu.edu>
2015-01-05 07:26:38 -0500

    create view, imported forms

--- roomlist/accounts/views.py
+++ roomlist/accounts/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 
 from accounts.models import Student
+from accounts.forms import StudentForm
 
 from braces.views import LoginRequiredMixin
+
+# create a student
+class CreateStudent(LoginRequiredMixin, CreateView):
+    model = Student
+    form_class = StudentForm
+    success_url = '/' #redirect location tba
+    login_url = '/'
 
 # details about the student
 class DetailStudent(LoginRequiredMixin, DetailView):
