10d4ac4d4e6ea3ac325058f5827874f72b4860bb
Daniel W Bond <dbond2@gmu.edu>
2015-01-11 00:22:33 -0500

    redirected contact page to srct contact page

--- roomlist/settings/urls.py
+++ roomlist/settings/urls.py
@@ -12,7 +12,6 @@ urlpatterns = patterns('',
     # project-level urls
     url(r'^$', TemplateView.as_view( template_name="index.html" ), name='index'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
-    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^privacy/$', TemplateView.as_view(template_name='privacy.html'), name='privacy'),
 
     # app-level urls
