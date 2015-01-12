2ba9fc87cf33f7a453d6704a296ec529029cdcb0
Jason D Yeomans <jyeoman2@gmu.edu>
2015-01-12 01:41:00 -0500

    studentSettings page templatising

--- roomlist/settings/settings.py
+++ roomlist/settings/settings.py
@@ -78,6 +78,7 @@ INSTALLED_APPS = (
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.twitter',
+    'allauth.socialaccount.providers.instagram',
 )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap'
