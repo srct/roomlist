6b8c54a349509e2223c2848de8d98f0837eb176a
Jason D Yeomans <jyeoman2@gmu.edu>
2015-01-13 17:43:48 -0500

    fix admin access

--- roomlist/settings/urls.py
+++ roomlist/settings/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 from django.contrib import admin
 
 admin.autodiscover()
+admin.site.login = login_required(admin.site.login)
 
 handle404 = TemplateView.as_view(template_name="404.html")
 handle500 = TemplateView.as_view(template_name="500.html")
@@ -20,8 +22,11 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('accounts.urls')),
 
     # login and logout
-    url(r'^login/$', 'cas.views.login', name='login'),
-    url(r'^logout/$', 'cas.views.logout', name='logout'),
+    #url(r'^login/', 'cas.views.login', name='login'),
+    #url(r'^logout/', 'cas.views.logout', name='logout'),
+
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
 
     # alternate interfaces
     url(r'^api/', include('api.urls')),
