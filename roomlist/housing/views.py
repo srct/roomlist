4d7b734043a8c7cd0aba262d2e2e5b98f8ae678f
Daniel W Bond <dbond2@gmu.edu>
2015-02-09 21:48:22 -0500

    floor views and modified filters

--- roomlist/housing/views.py
+++ roomlist/housing/views.py
@@ -25,32 +25,33 @@ class DetailBuilding(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(DetailBuilding, self).get_context_data(**kwargs)
-        context['room_list'] = Room.objects.filter(building__name=''+self.get_object().name).order_by('number')
+        context['floors'] = Floor.objects.filter(building__name=''+self.get_object().name_.order_by('-number')
         return context
+
     login_url = '/'
 
 # this lists the rooms on the floor
-class ListRooms(LoginRequiredMixin, ListView):
-    model = Room
+class DetailFloor(LoginRequiredMixin, DetailView):
+    model = Floor
+    context_object_name = 'floor'
+    template_name = detail_floor.html
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailFloor, self):
+        context['rooms'] = Room.objects.filter(floor=self.get_object()).order_by('-number')
+        return context
+
     login_url = '/'
 
 # this lists students in a room
 class DetailRoom(LoginRequiredMixin, ListView):
     model = Room
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailRoom, self):
+#        context['students'] = 
+        return context
+
     login_url = '/'
 
-# update a student
-#class UpdateStudent(LoginRequiredMixin, UpdateView):
-#    model = Student
-#    form_class = '/'
-#    success_url = '/' # change the success url to something more interesting
-
-#    login_url = '/'
-
-# update a room
-#class updateroom(loginrequiredmixin, updateview):
-#    model = room
-#    form_class =
-#    success_url = '/' # change the success url to something more interesting
-
-#    login_url = '/'
+# deleted 'updateroom' view-- that will be handled on the user's page
