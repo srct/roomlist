6941d8147812474e480954ab33a3d234c605916e
Daniel W Bond <dbond2@gmu.edu>
2015-02-18 15:11:39 -0500

    querysets for student

--- roomlist/accounts/models.py
+++ roomlist/accounts/models.py
@@ -9,10 +9,53 @@ from housing.models import Building, Room, Class
 from allauth.socialaccount.models import SocialAccount
 import hashlib
 
-# Create your models here.
 class Major(TimeStampedModel):
     name = models.CharField(max_length = 50)
     # I believe the longest is "Government and International Politics"
+
+class StudentQuerySet(models.query.QuerySet):
+    def floor(self):
+        return self.filter(privacy=FLOOR)
+
+    def building(self):
+        return self.filter(privacy=BUILDING)
+
+    def students(self):
+        return self.filter(privacy=STUDENTS)
+
+class StudentManager(models.Manager):
+    def get_query_set(self):
+        return StudentQuerySet(self.model, using=self._db)
+
+    def floor(self):
+        return self.get_query_set().floor()
+
+    def building(self):
+        return self.get_query_set().building()
+
+    def students(self):
+        return self.get_query_set().students()
+
+    def floor_building(self):
+        floor = self.get_query_set().floor()
+        building = self.get_query_set().building()
+        return floor + list(set(building) - set(floor))
+
+    # when a student is not on a floor, but in a building
+    def building_students(self):
+        building = self.get_query_set().building()
+        students = self.get_query_set().students()
+        return building + list(set(students) - set(building))
+ 
+    # when a student is on a floor
+    def floor_building_students(self):
+        floor = self.get_query_set().floor()
+        building = self.get_query_set().building()
+        students = self.get_query_set().students()
+
+        building_students = building_students()
+
+        return floor + list(set(building_students) - set(floor))
 
 class Student(TimeStampedModel):
     user = models.OneToOneField(User)
@@ -37,6 +80,8 @@ class Student(TimeStampedModel):
     # social media accounts
 
     slug = AutoSlugField(populate_from='user', unique=True)
+
+    objects = StudentManager()
 
     def profile_image_url(self):
         fb_uid = SocialAccount.objects.filter(user=self.user.id, provider='facebook')
