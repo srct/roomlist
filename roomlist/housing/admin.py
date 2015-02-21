61a5c12301bedbf7b08fc96be4a1e64f301f6f3b
Daniel W Bond <dbond2@gmu.edu>
2015-02-21 11:21:44 -0500

    registered Floor

--- roomlist/housing/admin.py
+++ roomlist/housing/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from housing.models import Class, Building, Room
+from housing.models import Class, Building, Floor, Room
 
 # Register your models here.
 admin.site.register(Class)
 admin.site.register(Building)
+admin.site.register(Floor)
 admin.site.register(Room)
