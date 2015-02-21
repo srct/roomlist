c6d42c6b3bc48e8b96bf519de07b9688981b2bb9
Daniel W Bond <dbond2@gmu.edu>
2015-02-21 17:06:43 -0500

    added get_absolute_url

--- roomlist/housing/urls.py
+++ roomlist/housing/urls.py
@@ -22,17 +22,17 @@ urlpatterns = patterns('',
             template_name='detail_building.html'),
         name='detail_building'),
 
-#    url(r'^buildings/whitetop/(?P<slug>[\w-]+)/$',
-#        DetailFloor.as_view(
-#            model=Floor,
-#            context_object_name='floor',
-#            template_name='detail_floor.html'),
-#        name='detail_floor'),
+    url(r'^buildings/(?P<building>[\w-]+)/(?P<slug>[\w-]+)/$',
+        DetailFloor.as_view(
+            model=Floor,
+            context_object_name='floor',
+            template_name='detail_floor.html'),
+        name='detail_floor'),
 
-#    url(r'^buildings/whitetop/5/5542/$',
-#        DetailRoom.as_view(
-#            model=Room,
-#            context_object_name='room',
-#            template_name='detail_room.html'),
-#        name='detail_room'),
+    url(r'^buildings/(?P<building>[\w-]+)/(?P<floor>[\w-]+)/(?P<slug>[\w-]+)/$',
+        DetailRoom.as_view(
+            model=Room,
+            context_object_name='room',
+            template_name='detail_room.html'),
+        name='detail_room'),
 )
