07f11596d6b520a50e1db473614a2adf51a247be
Daniel W Bond <dbond2@gmu.edu>
2015-02-22 18:35:48 -0500

    shortened housing urls

--- roomlist/housing/urls.py
+++ roomlist/housing/urls.py
@@ -5,7 +5,7 @@ from housing.models import Building, Floor, Room
 
 urlpatterns = patterns('',
 
-    url(r'^buildings/$',
+    url(r'^$',
         ListBuildings.as_view(
             model=Building,
             #paginate_by='10',
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
             template_name='list_buildings.html'),
         name='list_buildings'),
 
-    url(r'^buildings/(?P<slug>[\w-]+)/$',
+    url(r'^(?P<slug>[\w-]+)/$',
         DetailBuilding.as_view(
             model=Building,
             slug_field='slug__iexact',
@@ -22,14 +22,14 @@ urlpatterns = patterns('',
             template_name='detail_building.html'),
         name='detail_building'),
 
-    url(r'^buildings/(?P<building>[\w-]+)/(?P<slug>[\w-]+)/$',
+    url(r'^(?P<building>[\w-]+)/(?P<slug>[\w-]+)/$',
         DetailFloor.as_view(
             model=Floor,
             context_object_name='floor',
             template_name='detail_floor.html'),
         name='detail_floor'),
 
-    url(r'^buildings/(?P<building>[\w-]+)/(?P<floor>[\w-]+)/(?P<slug>[\w-]+)/$',
+    url(r'^(?P<building>[\w-]+)/(?P<floor>[\w-]+)/(?P<slug>[\w-]+)/$',
         DetailRoom.as_view(
             model=Room,
             context_object_name='room',
