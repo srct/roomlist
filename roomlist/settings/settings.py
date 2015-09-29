2467fa7683af7ee9eb72acefbcd898f155103977
Daniel W Bond <dbond2@gmu.edu>
2015-09-29 13:45:20 -0400

    added site id comment, peoplefinder url

--- roomlist/settings/settings.py
+++ roomlist/settings/settings.py
@@ -135,7 +135,8 @@ USE_L10N = True
 USE_TZ = True
 
 # set for social auth
-SITE_ID = 1
+# example.com is by default set to 1
+SITE_ID = 2
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
@@ -148,6 +149,9 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'cas.backends.CASBackend',
 )
+
+# Peoplefinder API for user creation
+PF_URL = "http://api.srct.gmu.edu/pf/v1/"
 
 CAS_SERVER_URL = 'https://login.gmu.edu'
 CAS_LOGOUT_COMPLETELY = True
