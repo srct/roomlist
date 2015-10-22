b0e081f1bf37a69ea8f85761c6d373493a9a7c29
Briana Abraham <babraha2@gmu.edu>
2015-10-21 20:48:32 -0400

    clears student rooms at the end of the semester

---
+++ roomlist/accounts/management/commands/clear_rooms.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand, CommandError
+from accounts.models import Student 
+
+class Command(BaseCommand):
+    args = ""
+    help = "Deletes all students' rooms at the end of the semester"
+    
+    def handle(self, *args, **kwargs):
+        count = 0
+        for student in Student.objects.all():
+            student.room = None
+	    count += 1
+        self.stdout.write("Successfully Overwrote %d student room(s)" % count)
