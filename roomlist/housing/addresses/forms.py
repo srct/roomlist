2723f74def65ea9f5c0646b9c83bd653fcc84863
Daniel W Bond <dbond2@gmu.edu>
2016-01-28 12:40:27 -0500

    removed commented-out address model from housing.models and put in separate directory along with other address material

---
+++ roomlist/housing/addresses/forms.py
@@ -0,0 +1,31 @@
+# standard library imports
+from __future__ import absolute_import, print_function
+# core django imports
+from django import forms
+from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
+from localflavor.us.us_states import US_STATES as states
+# imports from your apps
+
+
+#class YesNoBooleanWidget(forms.widget.RadioWidget):
+
+class AddressWidget(forms.widgets.Widget):
+
+    template_name = 'address_widget.html'
+
+    def __init__(self, user=None, attrs=None):
+        super(AddressWidget, self).__init__(attrs)
+
+    def render(self, name, value, attrs=None):
+        # I'm sure I should be elated about i18n, but not feeling that way at the moment
+        actual_strings = [(abbr, unicode(state_name)) for abbr, state_name in states]
+        context = {
+            'states': actual_strings,
+        }
+        return mark_safe(render_to_string(self.template_name, context))
+
+
+class AddressField(forms.CharField):
+
+    widget = AddressWidget
