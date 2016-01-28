2f9e938e71fa725ca7ab1816d032e68bfd65f888
Daniel W Bond <dbond2@gmu.edu>
2016-01-28 12:49:12 -0500

    created templatetag to add gender icons to gender names

--- roomlist/accounts/templatetags/accounts_extras.py
+++ roomlist/accounts/templatetags/accounts_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -11,3 +12,21 @@ def bc(num):
         return "%s B.C." % (positive_value)
     else:
         return num
+
+
+@register.filter(name='gender_icon')
+def gender_icon(gender_name):
+    icon_template = '<i class="fa fa-%s fa-fw"></i>'
+    if gender_name == 'male':
+        icon_tag = (icon_template % 'mars') + " Male"
+    elif gender_name == 'female':
+        icon_tag = (icon_template % 'venus') + " Female"
+    elif gender_name == 'trans':
+        icon_tag = (icon_template % 'transgender') + " Trans"
+    elif gender_name == 'intersex':
+        icon_tag = (icon_template % 'transgender-alt') + " Intersex"
+    elif gender_name == 'genderless':
+        icon_tag = (icon_template % 'genderless') + " Genderless"
+    elif gender_name == 'other':
+        icon_tag = (icon_template % 'mars-stroke-v') + " Other"
+    return mark_safe(icon_tag)
