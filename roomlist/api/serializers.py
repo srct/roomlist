f3deb44d27fb490ed062d5e61e22c51ac6a1a59b
Daniel W Bond <dbond2@gmu.edu>
2016-02-08 11:21:43 -0500

    begun fleshing out serializers, added major serializer

--- roomlist/api/serializers.py
+++ roomlist/api/serializers.py
@@ -1,12 +1,18 @@
 # standard library imports
 from __future__ import absolute_import, print_function
+# core django imports
+from django.core.serializers import serialize
 # third party imports
 from rest_framework import serializers
 # imports from your apps
 from housing.models import Building, Floor, Room
+from accounts.models import Major
 
 
 class BuildingSerializer(serializers.ModelSerializer):
+
+    # floors
+    # rooms
 
     class Meta:
         model = Building
@@ -15,13 +21,40 @@ class BuildingSerializer(serializers.ModelSerializer):
 
 class FloorSerializer(serializers.ModelSerializer):
 
+    building = serializers.SerializerMethodField('get_building_name')
+    #rooms = serializers.SerializerMethodField('get_floor_rooms')
+
+    def get_building_name(self, floor):
+        return floor.building.name
+
+    def get_floor_rooms(self, floor):
+        rooms = serialize('json', Room.objects.filter(floor=floor),
+                          fields=('number'))
+        return rooms
+
     class Meta:
         model = Floor
-        fields = ('number', )
+        fields = ('number', 'building', )#'rooms')
 
 
 class RoomSerializer(serializers.ModelSerializer):
 
+    building = serializers.SerializerMethodField('get_building_name')
+    floor = serializers.SerializerMethodField('get_floor_number')
+
+    def get_building_name(self, room):
+        return room.floor.building.name
+
+    def get_floor_number(self, room):
+        return room.floor.number
+
     class Meta:
         model = Room
-        fields = ('number', )
+        fields = ('number', 'floor', 'building')
+
+
+class MajorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Major
+        fields = ('name', )
