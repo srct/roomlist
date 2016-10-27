35ae80f96811cccf386ffb909ae55d1b39a8524b
Daniel W Bond <dbond2@gmu.edu>
2016-10-27 17:56:44 -0400

    minor tweaks to welcome back wording, just because I was already looking at those for fixing the double-message problem

--- roomlist/accounts/student_messages.py
+++ roomlist/accounts/student_messages.py
@@ -1,22 +1,22 @@
 bug_reporting = """Welcome back to SRCT Roomlist. This project is the
                    <a href="https://srct.gmu.edu/projects/">collaborative work
-                   of students like you</a>. If you see anything amiss, or have ideas for
-                   features or a better user experience, please send an email to
-                   roomlist@lists.srct.gmu.edu, tweet
+                   of students like you</a>. If you see anything amiss, or have ideas
+                   for features or a better user experience, please email
+                   <a href="mailto:roomlist@lists.srct.gmu.edu">
+                   roomlist@lists.srct.gmu.edu</a>, tweet
                    <a href="https://twitter.com/MasonSRCT/">@MasonSRCT</a>, or, for the
                    more technically experienced, review our
                    <a href="https://git.gmu.edu/srct/roomlist/issues">issues page</a>."""
 
-privacy_reminder = """Welcome back to SRCT Roomlist. A friendly reminder you can change
-                      your privacy settings at any time on your settings page by
-                      clicking the cog in the upper right of your screen."""
+privacy_reminder = """Welcome back to SRCT Roomlist. A friendly reminder that you can
+                      change your privacy settings at any time by clicking the cog in
+                      the upper right of your screen."""
 
-disclaimer = """Welcome back to SRCT Roomlist. Just to be perfectly clear, this project
-                is provided as a service by the
-                <a href="https://gmu.collegiatelink.net/organization/srct">registered
-                student organization</a>
-                <a href="https://srct.gmu.edu/">Student-Run Computing and Technology</a>.
-                We are not a part of <a href="http://housing.gmu.edu/">Mason Housing</a>:
+disclaimer = """Welcome back to SRCT Roomlist. This project was written for the benefit
+                of the Mason community by <a href="https://srct.gmu.edu/">SRCT</a>,
+                a <a href="https://gmu.collegiatelink.net/organization/srct">
+                registered student organization</a>. We are not affiliated with
+                <a href="http://housing.gmu.edu/">Mason Housing</a> and
                 all information is voluntarily provided by participating students."""
 
 whatsopen_plug = """Welcome back to SRCT Roomlist. Wondering what's open at this hour?
@@ -29,8 +29,8 @@ open_source = """Welcome back to SRCT Roomlist. For the curious at heart,
                  <a href="http://www.gnu.org/philosophy/free-sw.en.html">you can always
                  review</a> this project's
                  <a href="https://git.gmu.edu/srct/roomlist/tree/master">source code</a>.
-                 Come <a href="https://srct.gmu.edu/">to a meeting</a> and learn how to
-                 contribute!"""
+                 Come <a href="https://srct.gmu.edu/calendar">to a meeting</a> and learn
+                 how to contribute!"""
 
 return_messages = [bug_reporting, privacy_reminder, disclaimer,
                    whatsopen_plug, open_source]
