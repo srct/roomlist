5ceb42cd29eae0717a5f15385a6a99b835d94488
Daniel W Bond <dbond2@gmu.edu>
2017-05-26 18:29:50 -0400

    created mixin to add 'student' to get_context_data

--- roomlist/core/views.py
+++ roomlist/core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# standard library imports
+from __future__ import absolute_import, print_function
+# core django imports
+from django.views.generic.base import ContextMixin
 
-# Create your views here.
+
+class StudentContextMixin(ContextMixin):
+    def get_context_data(self, **kwargs):
+        context = super(StudentContextMixin, self).get_context_data(**kwargs)
+
+        me = self.request.user.student
+
+        context['student'] = me
+
+        return context
