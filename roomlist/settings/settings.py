92cf7743e3828e76090fff98d8e39ee7ed03fd69
Daniel W Bond <dbond2@gmu.edu>
2017-06-20 14:12:11 -0400

    added middleware to project settings

--- roomlist/settings/settings.py
+++ roomlist/settings/settings.py
@@ -114,6 +114,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'settings.views.BlockingMiddleware',
 )
 
 ROOT_URLCONF = 'settings.urls'
